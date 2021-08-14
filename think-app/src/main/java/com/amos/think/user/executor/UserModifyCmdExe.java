256a7f38dc7f1b92bc3be8f5322e5c2814aefb1a
Yangke <yangke0227@gmail.com>
2021-08-14 06:59:40 +0000

    修改为SSH方式提交

--- think-app/src/main/java/com/amos/think/user/executor/UserModifyCmdExe.java
+++ think-app/src/main/java/com/amos/think/user/executor/UserModifyCmdExe.java
@@ -14,9 +14,6 @@ import javax.annotation.Resource;
 
 /**
  * DESCRIPTION: UserAddCmdExe
- *
- * @author <a href="mailto:daoyuan0626@gmail.com">amos.wang</a>
- * @date 2021/1/10
  */
 @Component
 public class UserModifyCmdExe {
