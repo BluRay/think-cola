74c6a28b5864c0ee15a03b76b7d32b6a47cadd99
yangke0227@gmail.com <yangke0227@gmail.com>
2025-03-06 23:34:21 +0800

    Update FuturesController.java

--- think-controller/src/main/java/com/amos/think/web/FuturesController.java
+++ think-controller/src/main/java/com/amos/think/web/FuturesController.java
@@ -5,8 +5,12 @@ import com.alibaba.cola.dto.SingleResponse;
 import com.amos.think.api.IFuturesService;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
 import javax.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +27,24 @@ public class FuturesController {
     data.put("code", 0);
     data.put("result", futuresService.getAccountPageList(parmsMap));
     data.put("message", "查询成功");
+    data.put("type", "success");
+    return SingleResponse.of(data);
+  }
+  @SuppressWarnings("unchecked")
+  @PostMapping(value = "/uploadAccount")
+  public Response uploadAccount(@RequestBody Map<String, Object> parmsMap) {
+    System.out.println("---->futures::uploadAccount");
+    List<List<String>> list = (List<List<String>>) parmsMap.get("upload_data");
+    
+    for(int i=1; i < list.size() - 1; i++) {
+      System.out.println(list.get(i).get(2));
+    }
+    Map<String, Object> account = new HashMap<>();
+    
+
+    Map<String, Object> data = new HashMap<>();
+    data.put("code", 0);
+    data.put("message", "导入成功");
     data.put("type", "success");
     return SingleResponse.of(data);
   }
